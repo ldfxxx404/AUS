5f7c5b7f5e35651301c94524e9a4967d20674fc7
ldfxxx404 <limur9990@gmail.com>
2024-08-25 11:26:27 +0300

    added update script

---
+++ autoupdate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
+echo -e "\n${PURPLE}AUS v 1.1 Ɑ͞ ̶͞ ̶͞ ̶͞ لں͞   ${NC}\n"
+
+echo -e "${PURPLE}Updating the Gentoo repo...${NC}\n"
+emaint --auto sync
+
+echo -e "${PURPLE}World updating...${NC}\n"
+emerge -avuDN --with-bdeps=y @world
+
+echo -e "${PURPLE}Cleaning unnecessary packages${NC}\n"
+emerge --depclean
+
+echo -e "${PURPLE}Cleaning source files archives${NC}\n"
+eclean-dist --destructive
+
+echo -e "${PURPLE}Cleaning binary packages${NC}\n" 
+eclean-pkg --destructive
+
+echo -e "${PURPLE}System was updated successfully ≽^•⩊•^≼ ${NC}\n"
