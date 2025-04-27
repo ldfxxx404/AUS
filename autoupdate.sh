d5bbabcfdd4708765dbac8f06f0e312f5c22b93c
ldfxxx404 <limur9990@gmail.com>
2025-04-27 20:29:12 +0300

    chroe: code improvement

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,38 +4,72 @@ PURPLE='\033[0;35m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+EFI_DIR="/boot"
+
+check_command_success() {
+    if [ $? -ne 0 ]; then
+        echo -e "${PURPLE}Error: Command failed. Exiting...${NC}"
+        exit 1
+    fi
+}
+
+update_gentoo() {
+    echo -e "${PURPLE}Updating the Gentoo repo...${NC}"
+    emaint --auto sync
+    check_command_success
+
+    echo -e "${PURPLE}World updating...${NC}"
+    emerge -avuDN --with-bdeps=y @world
+    check_command_success
+}
+
+clean_system() {
+    echo -e "${PURPLE}Cleaning unnecessary packages${NC}"
+    emerge --depclean
+    check_command_success
+
+    echo -e "${PURPLE}Cleaning source files archives${NC}"
+    eclean-dist --destructive
+    check_command_success
+
+    echo -e "${PURPLE}Cleaning binary packages${NC}"
+    eclean-pkg --destructive
+    check_command_success
+}
+
+update_grub() {
+    
+    if [ "$(id -u)" -ne 0 ]; then
+        echo -e "${PURPLE}This script must be run as root to update GRUB.${NC}"
+        exit 1
+    fi
+
+    echo -e "${PURPLE}Updating GRUB...${NC}"
+    grub-install --efi-directory="$EFI_DIR"
+    check_command_success
+
+    grub-mkconfig -o /boot/grub/grub.cfg
+    check_command_success
+
+    echo -e "${PURPLE}GRUB was updated${NC}"
+}
+
 echo -e "\n${PURPLE}AUS v 1.1${NC}\n"
 
-echo -e "${PURPLE}Updating the Gentoo repo...${NC}\n"
-emaint --auto sync
+update_gentoo
 
-echo -e "${PURPLE}World updating...${NC}\n"
-emerge -avuDN --with-bdeps=y @world
-
-echo -e "${PURPLE}Cleaning unnecessary packages${NC}\n"
-emerge --depclean
-
-echo -e "${PURPLE}Cleaning source files archives${NC}\n"
-eclean-dist --destructive
-
-echo -e "${PURPLE}Cleaning binary packages${NC}\n" 
-eclean-pkg --destructive
+clean_system
 
 echo -e "${PURPLE}System was updated successfully ≽^•⩊•^≼ ${NC}\n"
 
-#GRUB update step
-
 echo -e "${GREEN}Update GRUB? y - yes or n - no${NC}"
-echo -en "${NC}"
 read -p "yes/no: " choose
 
-if [[ "$choose" == 'yes' || "$choose" == 'y' ]]; then
-    echo -e "${PURPLE}Updating GRUB...${NC}\n"
-    grub-install --efi-directory=/boot
-    grub-mkconfig -o /boot/grub/grub.cfg
-    echo -e "\n${PURPLE}GRUB was updated${NC}\n"
+if [[ "$choose" =~ ^(yes|y)$ ]]; then
+    update_grub
 else 
-    echo -e "GRUB update was aborted."
+    echo -e "${PURPLE}GRUB update was aborted.${NC}"
 fi
 
 exit 0
+
